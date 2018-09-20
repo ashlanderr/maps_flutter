c32dc78dd5dcb58c4c543e18ce85a3098d170c14
Александр Шилов <aleksandr.schilow2012@gmail.com>
2018-09-20 16:00:46 +0300

    Add MapBox tile provider

--- lib/tiles.dart
+++ lib/tiles.dart
@@ -18,3 +18,19 @@ class OpenStreetMap implements TileProvider {
     return frame.image;
   }
 }
+
+class MapBox implements TileProvider {
+  final String accessToken;
+
+  MapBox(this.accessToken);
+
+  @override
+  Future<Image> fetch(int level, int x, int y) async {
+    final url = "https://api.tiles.mapbox.com/v4/mapbox.streets/$level/$x/$y@2x.png?access_token=$accessToken";
+    final response = await http.get(url);
+    final bytes = response.bodyBytes;
+    final codec = await instantiateImageCodec(bytes);
+    final frame = await codec.getNextFrame();
+    return frame.image;
+  }
+}
